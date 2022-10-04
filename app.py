cc0726dba8ac255b96a2aa0516b00ae41fcaf0fb
JakobSig95 <Jakobdransfeldt@gmail.com>
2022-10-04 17:43:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+import pickle
 
 st.write('Hello')
+
+# load the model from disk
+loaded_model = pickle.load(open('model_xgb.pkl', 'rb'))
+# result = loaded_model.score(X_test, y_test)
