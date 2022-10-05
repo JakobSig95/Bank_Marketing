2b9c0c0a9c03d413495c8c335171dc56cbf26ef6
Mikkelrune <112550996+Mikkelrune@users.noreply.github.com>
2022-10-05 14:03:55 +0200

    asfa

--- app.py
+++ app.py
@@ -91,9 +91,3 @@ with st.sidebar:
     
 with tab2: 
 
-    
-
-# load the model from disk
-#loaded_model = pickle.load(open('model_xgb.pkl', 'rb'))
-# result = loaded_model.score(X_test, y_test)
-# y_pred = loaded_model.predict(X_test)
